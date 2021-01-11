8ac9bb5117a5b7b7dd3e6d1081e074ec51ef06d8
Cris Silva Jr <crisjr@pm.me>
2021-01-11 11:40:26 -0300

    Refactor image storage

--- mandelbrot.go
+++ mandelbrot.go
@@ -41,7 +41,7 @@ func Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width floa
 func Escapes(cx, cy float64) bool {
     zx := 0.0
     zy := 0.0
-    maxItr := 10000  // TODO turn this into a constant
+    maxItr := 1024  // TODO turn this into a constant
 
     for i:= 0; i < maxItr && math.Hypot(zx, zy) <= 2.0; i++ {
         temp := zx*zx - zy*zy + cx
@@ -65,7 +65,7 @@ func setPixel(fractal [][]bool, frameInitX, frameInitY, frameEndX, frameEndY, he
 }
 
 func ZoomAt(posX, posY, width, height, frameInitX, frameInitY, frameEndX, frameEndY float64) (float64, float64, float64, float64) {
-    factor := 0.6  // TODO turn this into a constant
+    factor := 0.9  // TODO turn this into a constant
     centerX := frameInitX + (posX * (frameEndX - frameInitX)) / (width)
     centerY := frameInitY + (posY * (frameEndY - frameInitY)) / (height)
     distX := factor * (frameEndX - frameInitX) / 2
