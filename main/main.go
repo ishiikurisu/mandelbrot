c1615b95b284f1c6fed9cd4b5335d4fb5cb897af
Cris Silva Jr <crisjr@pm.me>
2021-01-11 13:13:43 -0300

    Create video from images

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "github.com/ishiikurisu/mandelbrot"
     "github.com/ishiikurisu/mandelbrot/processing"
 )
@@ -8,13 +9,14 @@ import (
 func main() {
     var height float64 = 900.0
     var width float64 = 1600.0
+    var noIterations int = 100
     frameInitX, frameInitY, frameEndX, frameEndY := mandelbrot.FirstSetting(height, width)
-    fractal := mandelbrot.Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width)
-    img := processing.FractalToImage(fractal)
-    processing.SaveImage(img, "./out/m1.png")
 
-    frameInitX, frameInitY, frameEndX, frameEndY = mandelbrot.ZoomAt(width / 10, height / 2, width, height,frameInitX, frameInitY, frameEndX, frameEndY)
-    fractal = mandelbrot.Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width)
-    img = processing.FractalToImage(fractal)
-    processing.SaveImage(img, "./out/m2.png")
+    for i := 0; i < noIterations; i++ {
+        fractal := mandelbrot.Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width)
+        img := processing.FractalToImage(fractal)
+        fn := fmt.Sprintf("./out/m%03d.png", i)
+        processing.SaveImage(img, fn)
+        frameInitX, frameInitY, frameEndX, frameEndY = mandelbrot.ZoomAt(width / 10, height / 2, width, height,frameInitX, frameInitY, frameEndX, frameEndY)
+    }
 }
--- makefile
+++ makefile
@@ -1,2 +1,11 @@
-default:
+default: run
+
+clean:
+	rm out/*.png
+	rm out/*.mp4
+
+run:
 	go run main/main.go
+
+video: clean run
+	ffmpeg -framerate 1 -pattern_type glob -i 'out/*.png' -c:v libx264 -r 30 -pix_fmt yuv420p out/mandelbrot.mp4
