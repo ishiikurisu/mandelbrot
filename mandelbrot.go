23328f8efb1537e7e4a01a91b5b9f0ab478d13e8
Cris Jr <crisjr@pm.me>
2021-01-24 20:24:47 -0300

    Convert operations to arbitrary arithmetic

--- mandelbrot.go
+++ mandelbrot.go
@@ -1,75 +1,190 @@
 package mandelbrot
 
 import (
-    "math"
+    "math/big"
 )
 
-func FirstSetting(height, width float64) (float64, float64, float64, float64) {
-    frameInitX := -width / height
-    frameInitY := -1.0
-    frameEndX := width / height
-    frameEndY := 1.0
+func NewFloat(f float64) *big.Float {
+    return new(big.Float).SetPrec(128).SetFloat64(f)
+}
 
-    if (height > width) {
-        frameInitX = -1.0
-        frameInitY = -height / width
-        frameEndX = 1.0
-        frameEndY = height / width
+func FirstSetting(height, width *big.Float) (*big.Float, *big.Float, *big.Float, *big.Float) {
+    acc := NewFloat(-1.0)
+    frameInitX := NewFloat(0.0)
+    frameInitY := NewFloat(0.0)
+    frameEndX := NewFloat(0.0)
+    frameEndY := NewFloat(0.0)
+
+    // frameInitX = - width / height
+    acc.Neg(width)
+    acc.Quo(acc, height)
+    frameInitX.Copy(acc)
+    frameInitY = NewFloat(-1.0)
+    // frameEndX = width / height
+    acc.Quo(width, height)
+    frameEndX.Copy(acc)
+    frameEndY = NewFloat(1.0)
+
+    if (height.Cmp(width) == 1) {  // height > width
+        frameInitX = NewFloat(-1.0)
+        // frameInitY = - height / width
+        acc = acc.Neg(height)
+        acc.Quo(acc, width)
+        frameInitY.Copy(acc)
+        frameEndX = NewFloat(1.0)
+        // frameEndY = height / width
+        acc.Quo(height, width)
+        frameEndY.Copy(acc)
     }
 
     return frameInitX, frameInitY, frameEndX, frameEndY
 }
 
-func Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width float64) [][]bool {
-    dx := (frameEndX - frameInitX) / width
-    dy := (frameEndY - frameInitY) / height
+func Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width *big.Float) [][]bool {
+    acc := NewFloat(0.0)
+    dx := NewFloat(0.0)
+    dy := NewFloat(0.0)
 
-    fractal := make([][]bool, int(width))
-    for i := 0; i < int(width); i++ {
-        fractal[i] = make([]bool, int(height))
+    // dx = (frameEndX - frameInitX) / width
+    acc.Sub(frameEndX, frameInitX)
+    acc.Quo(acc, width)
+    dx.Copy(acc)
+
+    // dy = (frameEndY - FrameInitY) / height
+    acc.Sub(frameEndY, frameInitY)
+    acc.Quo(acc, height)
+    dy.Copy(acc)
+
+    intWidth, _ := width.Int64()
+    intHeight, _ := height.Int64()
+    fractal := make([][]bool, intWidth)
+    var i int64
+    for i = 0; i < intWidth; i++ {
+        fractal[i] = make([]bool, intHeight)
     }
 
-    for y := frameInitY; y < frameEndY; y += dy {
-        for x := frameInitX; x < frameEndX; x += dx {
-            fractal = setPixel(fractal, frameInitX, frameInitY, frameEndX, frameEndY, height, width, x, y, Escapes(x, y))
+    y := NewFloat(0.0)
+    x := NewFloat(0.0)
+    for y.Copy(frameInitY); y.Cmp(frameEndY) == -1; y.Add(y, dy) {
+        for x.Copy(frameInitX); x.Cmp(frameEndX) == -1; x.Add(x, dx) {
+            escapes := Escapes(x, y)
+            fractal = setPixel(fractal, frameInitX, frameInitY, frameEndX, frameEndY, height, width, x, y, escapes)
         }
     }
 
     return fractal
 }
 
-func Escapes(cx, cy float64) bool {
-    zx := 0.0
-    zy := 0.0
+func Escapes(cx, cy *big.Float) bool {
+    zx := NewFloat(0.0)
+    zy := NewFloat(0.0)
+    two := NewFloat(2.0) // TODO turn this into a constant
+    limit := two.Copy(two)
     maxItr := 1024  // TODO turn this into a constant
 
-    for i:= 0; i < maxItr && math.Hypot(zx, zy) <= 2.0; i++ {
-        temp := zx*zx - zy*zy + cx
-        zy = 2*zx*zy + cy
-        zx = temp
+
+    for i := 0; i < maxItr && Pythagoras(zx, zy).Cmp(limit) == -1; i++ {
+        acc := NewFloat(0.0)
+        temp := NewFloat(0.0)
+        zx2 := NewFloat(0.0)
+        zy2 := NewFloat(0.0)
+
+        // zx = zx*zx - zy*zy + cx
+        zx2.Mul(zx, zx)
+        zy2.Mul(zy, zy)
+        acc.Sub(zx2, zy2)
+        acc.Add(acc, cx)
+        temp.Copy(acc)
+
+        // zy = 2 * zx * zy + cy
+        acc.Mul(two, zx)
+        acc.Mul(acc, zy)
+        acc.Add(acc, cy)
+
+        zx.Copy(temp)
+        zy.Copy(acc)
     }
 
-    return math.Hypot(zx, zy) > 2
+    return Pythagoras(zx, zy).Cmp(two) == 1
 }
 
-func setPixel(fractal [][]bool, frameInitX, frameInitY, frameEndX, frameEndY, height, width, px, py float64, state bool) [][]bool {
-    x := ((px - frameInitX) * width) / (frameEndX - frameInitX)
-    y := ((py - frameInitY) * height) / (frameEndY - frameInitY)
+func Pythagoras(x, y *big.Float) *big.Float {
+    x2 := NewFloat(0.0)
+    y2 := NewFloat(0.0)
 
-    i := int(math.Floor(x))
-    j := int(math.Floor(y))
+    x2.Mul(x, x)
+    y2.Mul(y, y)
+    x2.Add(x2, y2)
 
+    return x2.Sqrt(x2)
+}
+
+func setPixel(fractal [][]bool, frameInitX, frameInitY, frameEndX, frameEndY, height, width, px, py *big.Float, state bool) [][]bool {
+    accA := NewFloat(0.0)
+    accB := NewFloat(0.0)
+    x := NewFloat(0.0)
+    y := NewFloat(0.0)
+
+    // float posX = ((px - FRAME_INIT_X) * width) / (FRAME_END_X - FRAME_INIT_X);
+    accA.Sub(px, frameInitX)
+    accA.Mul(accA, width)
+    accB.Sub(frameEndX, frameInitX)
+    accA.Quo(accA, accB)
+    x.Copy(accA)
+
+    // float posY = ((py - FRAME_INIT_Y) * height) / (FRAME_END_Y - FRAME_INIT_Y);
+    accA.Sub(py, frameInitY)
+    accA.Mul(accA, height)
+    accB.Sub(frameEndY, frameInitY)
+    accA.Quo(accA, accB)
+    y.Copy(accA)
+
+    i, _ := x.Int64()
+    j, _ := y.Int64()
     fractal[i][j] = state
 
     return fractal
 }
 
-func ZoomAt(posX, posY, width, height, frameInitX, frameInitY, frameEndX, frameEndY float64) (float64, float64, float64, float64) {
-    factor := 0.9  // TODO turn this into a constant
-    centerX := frameInitX + (posX * (frameEndX - frameInitX)) / (width)
-    centerY := frameInitY + (posY * (frameEndY - frameInitY)) / (height)
-    distX := factor * (frameEndX - frameInitX) / 2
-    distY := factor * (frameEndY - frameInitY) / 2
-    // return frameInitX, frameInitY, frameEndX, frameEndY
-    return centerX - distX, centerY - distY, centerX + distX, centerY + distY
+func ZoomAt(posX, posY, width, height, frameInitX, frameInitY, frameEndX, frameEndY *big.Float) (*big.Float, *big.Float, *big.Float, *big.Float) {
+    factor := NewFloat(0.9)  // TODO turn this into a constant
+    two := NewFloat(2.0)  // TODO turn this into a constant
+    acc := NewFloat(0.0)
+    centerX := NewFloat(0.0)
+    centerY := NewFloat(0.0)
+    distX := NewFloat(0.0)
+    distY := NewFloat(0.0)
+
+    // float centerX = FRAME_INIT_X + (posX * (FRAME_END_X - FRAME_INIT_X)) / (width);
+    acc.Sub(frameEndX, frameInitX)
+    acc.Mul(acc, posX)
+    acc.Quo(acc, width)
+    acc.Add(frameInitX, acc)
+    centerX.Copy(acc)
+
+    // float centerY = FRAME_INIT_Y - (posY * (FRAME_END_Y - FRAME_INIT_Y)) / (height);
+    acc.Sub(frameEndY, frameInitY)
+    acc.Mul(acc, posY)
+    acc.Quo(acc, height)
+    acc.Add(frameInitY, acc)
+    centerY.Copy(acc)
+
+    // float distX = FACTOR * (FRAME_END_X - FRAME_INIT_X) / 2;
+    acc.Sub(frameEndX, frameInitX)
+    acc.Mul(factor, acc)
+    acc.Quo(acc, two)
+    distX.Copy(acc)
+
+    // float distY = FACTOR * (FRAME_END_Y - FRAME_INIT_Y) / 2;
+    acc.Sub(frameEndY, frameInitY)
+    acc.Mul(factor, acc)
+    acc.Quo(acc, two)
+    distY.Copy(acc)
+
+    frameInitX.Sub(centerX, distX)
+    frameInitY.Sub(centerY, distY)
+    frameEndX.Add(centerX, distX)
+    frameEndY.Add(centerY, distY)
+
+    return frameInitX, frameInitY, frameEndX, frameEndY
 }
