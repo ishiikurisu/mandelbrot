52904ef48b194050697737576bab9b15489993d8
Cris Jr <crisjr@pm.me>
2021-01-28 17:53:47 -0300

    Parallelize computation of many fractals

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "github.com/ishiikurisu/mandelbrot"
     "github.com/ishiikurisu/mandelbrot/processing"
+    "sync"
 )
 
 func main() {
@@ -14,8 +15,11 @@ func main() {
     factor := mandelbrot.NewFloat(0.9)
     frameInitX, frameInitY, frameEndX, frameEndY := mandelbrot.FirstSetting(height, width)
 
-    var noIterations int = 100
-    var noSkip int = 8
+    const noIterations int = 100
+    const noSkip int = 8
+    const totalGroups int = 6
+    var groups int = 0
+    var wg sync.WaitGroup
 
     for i := 0; i < noSkip; i++ {
         fmt.Printf("s%03d\n", i)
@@ -24,16 +28,39 @@ func main() {
     }
 
     for i := noSkip; i < noIterations; i++ {
-        fmt.Printf("m%03d\n", i)
+        // allocating memory
+        f0x := mandelbrot.NewFloat(0.0)
+        f0y := mandelbrot.NewFloat(0.0)
+        fx := mandelbrot.NewFloat(0.0)
+        fy := mandelbrot.NewFloat(0.0)
+        f0x.Copy(frameInitX)
+        f0y.Copy(frameInitY)
+        fx.Copy(frameEndX)
+        fy.Copy(frameEndY)
 
         // saving image
-        fractal := mandelbrot.Mandelbrot(frameInitX, frameInitY, frameEndX, frameEndY, height, width)
-        img := processing.FractalToImage(fractal)
-        fn := fmt.Sprintf("./out/m%03d.png", i)
-        processing.SaveImage(img, fn)
+        wg.Add(1)
+        go func(i int, wg *sync.WaitGroup) {
+            defer wg.Done()
+            fmt.Printf("m%03d\n", i)
+            fractal := mandelbrot.Mandelbrot(f0x, f0y, fx, fy, height, width)
+            img := processing.FractalToImage(fractal)
+            fn := fmt.Sprintf("./out/m%03d.png", i)
+            processing.SaveImage(img, fn)
+        }(i, &wg)
+        groups++
+
+        if groups == totalGroups {
+            wg.Wait()
+            groups = 0
+        }
 
         // zooming
         frameInitX, frameInitY, frameEndX, frameEndY = mandelbrot.Follow(
             targetX, targetY, factor, frameInitX, frameInitY, frameEndX, frameEndY)
+    }
+
+    if groups > 0 {
+        wg.Wait()
     }
 }
